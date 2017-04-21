b7e5c7893b5aa304c338fa4fd89e57cd26f68ea0
Emerson <emerson56@users.noreply.github.com>
2017-04-20 22:39:04 -0400

    Add files via upload

---
+++ Project0/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project0
+{
+	public class MainClass
+	{
+
+		public static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Welcome!");
+			Console.WriteLine("-----------------------");
+
+			// Initialize the HRManager class
+
+			// Add employee
+
+			// Add another employee
+
+			// Remove the first employee
+
+			// We should have just 1 employee
+
+			// Print count
+
+			// Print lone employee
+
+
+
+			// Find employee by first name
+			// Use the first name of the employee that is left
+
+			// Play w/ other methods that are in the HRManager class
+
+		}
+
+}
+
+	}
+
+
